0d655c4b7640e5ec00b8df35df387e663f8249a5
Gabriel Jones <gajones@archre.bm>
2025-03-07 19:15:13 -0400

    add link to mars game (not built yet). added social media buttons

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app.forms import ContactForm
-from flask import Blueprint, request, jsonify, abort, render_template
+from flask import Blueprint, request, jsonify, abort, render_template, send_from_directory
 
 
 blueprint = Blueprint('public', __name__)
@@ -13,3 +13,10 @@ def route():
 		print(form.name.data)
 
 	return render_template('index.html.j2', form=form)
+
+
+@blueprint.route('/mars', methods=['GET'])
+def mars():
+	print("MARS HERE")
+	return send_from_directory('static/mars-game/public', 'index.html')
+
